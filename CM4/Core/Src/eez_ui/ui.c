128b1a2b9de7bce9fb914e29c0a4950e7b59beba
alex <alexander.yakovlev@twerd.pl>
2026-07-02 12:36:29 +0200

    chore: update EEZ project file, assets and generated UI sources
    - EEZ/Riverdi-template/Riverdi-template.eez-project: updated project state
    - EEZ/Riverdi-template/assets/: new/updated image assets
    - CM4/Core/Src/eez_ui/ui.c, ui.h: regenerated from EEZ Studio

--- CM4/Core/Src/eez_ui/ui.c
+++ CM4/Core/Src/eez_ui/ui.c
@@ -1,18 +1,30 @@
 #include "ui.h"
-#include "lvgl/lvgl.h"
+#include "screens.h"
+#include "images.h"
+#include "actions.h"
+#include "vars.h"
+
+static int16_t currentScreen = -1;
+
+static lv_obj_t *getLvglObjectFromIndex(int32_t index) {
+    if (index == -1) {
+        return 0;
+    }
+    return ((lv_obj_t **)&objects)[index];
+}
+
+void loadScreen(enum ScreensEnum screenId) {
+    currentScreen = screenId - 1;
+    lv_obj_t *screen = getLvglObjectFromIndex(currentScreen);
+    lv_scr_load_anim(screen, LV_SCR_LOAD_ANIM_NONE, 0, 0, false);
+}
 
 void ui_init() {
-    /* Create dark blue background and centered label with text 'TWERD ENERGO-PLUS' */
-    lv_obj_t * scr = lv_scr_act();
-    lv_obj_set_style_bg_color(scr, lv_color_hex(0x00003B), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(scr, LV_OPA_COVER, LV_PART_MAIN);
+    create_screens();
+    loadScreen(SCREEN_ID_MAIN);
 
-    lv_obj_t * label = lv_label_create(scr);
-    lv_label_set_text(label, "TWERD ENERGO-PLUS");
-    lv_obj_set_style_text_color(label, lv_color_hex(0xFFFFFF), LV_PART_MAIN);
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 }
 
 void ui_tick() {
-    // Nothing to do in the dummy loop
+    tick_screen(currentScreen);
 }
--- CM4/Core/Src/eez_ui/ui.h
+++ CM4/Core/Src/eez_ui/ui.h
@@ -1,12 +1,18 @@
 #ifndef EEZ_LVGL_UI_GUI_H
 #define EEZ_LVGL_UI_GUI_H
 
+#include <lvgl/lvgl.h>
+
+#include "screens.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void ui_init();
 void ui_tick();
+
+void loadScreen(enum ScreensEnum screenId);
 
 #ifdef __cplusplus
 }
